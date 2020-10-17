11e954935a2008a9435cad4c85015f7f70680e56
Qz8954167 <357592895@qq.com>
2020-10-18 01:28:53 +0800

    feature
    创建API网关，路由到两个服务

---
+++ CatelogApi/Controllers/CurreniesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatelogApi.Controllers
+{
+    [ApiController]
+    [Route("api/currencies")]
+    public class CurreniesController : ControllerBase
+    {
+        // Get api/currencies
+        [HttpGet, Route("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "usd", "inr", "jpy" };
+        }
+    }
+}
